76837eb58ee056d966e095875b949c7a82e9b612
Robert Estelle <robertestelle@gmail.com>
2022-04-17 15:35:14 -0700

    oh-my-zsh.sh: err_unset compatibility

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -106,7 +106,7 @@ else
 fi
 
 # Save the location of the current completion dump file.
-if [[ -z "$ZSH_COMPDUMP" ]]; then
+if [[ -z "${ZSH_COMPDUMP:-}" ]]; then
   ZSH_COMPDUMP="${ZDOTDIR:-$HOME}/.zcompdump-${SHORT_HOST}-${ZSH_VERSION}"
 fi
 
@@ -121,7 +121,7 @@ if ! command grep -q -Fx "$zcompdump_revision" "$ZSH_COMPDUMP" 2>/dev/null \
   zcompdump_refresh=1
 fi
 
-if [[ "$ZSH_DISABLE_COMPFIX" != true ]]; then
+if [[ "${ZSH_DISABLE_COMPFIX:-}" != true ]]; then
   source "$ZSH/lib/compfix.zsh"
   # Load only from secure directories
   # Reset the flag compinit sets when -i excludes insecure entries
@@ -135,7 +135,7 @@ else
 fi
 
 # Append zcompdump metadata if missing
-if (( $zcompdump_refresh )) \
+if (( ${zcompdump_refresh:-0} )) \
   || ! command grep -q -Fx "$zcompdump_revision" "$ZSH_COMPDUMP" 2>/dev/null; then
   # Use `tee` in case the $ZSH_COMPDUMP filename is invalid, to silence the error
   # See https://github.com/ohmyzsh/ohmyzsh/commit/dd1a7269#commitcomment-39003489
